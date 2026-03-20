7f218d757422d28c0e2a6d9ce5ff2d03b9dfa581
Quyen Diep <2014datquyen@gmail.com>
2026-03-21 01:32:50 +0700

    Tạo database thực hành cho dự án Node.js ✔ Cấu hình Middleware Body-parser để có thể lấy dữ liệu từ Form ✔ Thêm mới người dùng (create users) với Node.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initwebRoutes from "./routes/web";
 require("dotenv").config();
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //config view engine
 configViewEngine(app);
