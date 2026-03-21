d8bbbb32986fd6854087861ec5eb3a244ef8b837
Quyen Diep <2014datquyen@gmail.com>
2026-03-21 18:49:31 +0700

    video 5.3 ✔ Sử dụng 'Redirect Page) với Express ✔ Sử dụng Form để Xóa Người dùng

--- src/service/userService.js
+++ src/service/userService.js
@@ -11,16 +11,18 @@ const hashPassword = (userPassword) => {
     return hashPassword;
 }
 
-const createNewUser = (email, password, username) => {
+const createNewUser = async (email, password, username) => {
     let hashPass = hashPassword(password);
-    connection.query(
-        'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, hashPass, username],
-        function (err, results, fields) {
-            if (err) {
-                console.log(err)
-            }
-        }
-    );
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+
+    try {
+        const [rows, fields] =
+            await connection.execute('INSERT INTO users (email, password, username) VALUES (?, ?, ?)',
+                [email, hashPass, username]);
+    } catch (error) {
+        console.log(">>> check error: ", error);
+    }
+
 }
 
 
@@ -29,19 +31,7 @@ const getUserList = async () => {
     // create the connection, specify bluebird as Promise
     const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
     let users = [];
-    // connection.query(
-    //     'Select * from users ',
-    //     function (err, results, fields) {
-    //         if (err) {
-    //             console.log(err);
-    //             return users;
-    //         }
 
-    //         users = results;
-    //         console.log(">>> run get user list:", users);
-    //         return users;
-    //     }
-    // );
     try {
         const [rows, fields] = await connection.execute('select * from users');
         return rows;
@@ -51,6 +41,20 @@ const getUserList = async () => {
     }
 }
 
+const deleteUser = async (id) => {
+
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+    let users = [];
+
+    try {
+        const [rows, fields] = await connection.execute('DELETE FROM users WHERE id=?', [id]);
+        return rows;
+        console.log(">>> check rows: ", rows);
+    } catch (error) {
+        console.log(">>> check error: ", error);
+    }
+}
+
 module.exports = {
-    createNewUser, getUserList
+    createNewUser, getUserList, deleteUser
 }
