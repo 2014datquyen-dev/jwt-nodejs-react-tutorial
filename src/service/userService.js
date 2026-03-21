e18fe99340dc110d016097ba9a8d3c66355a1a23
Quyen Diep <2014datquyen@gmail.com>
2026-03-21 21:09:01 +0700

    video 5.4 ✔ Tạo Page để Update user ✔ Fill các thông tin của người dùng vào Form ✔ Update thông tin của người dùng

--- src/service/userService.js
+++ src/service/userService.js
@@ -55,6 +55,32 @@ const deleteUser = async (id) => {
     }
 }
 
+const getUserById = async (id) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+    let users = [];
+
+    try {
+        const [rows, fields] = await connection.execute('Select * FROM users WHERE id=?', [id]);
+        console.log(">>> check rows: ", rows);
+        return rows;
+    } catch (error) {
+        console.log(">>> check error: ", error);
+    }
+}
+
+const updatUserInfor = async (email, username, id) => {
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+    let users = [];
+
+    try {
+        const [rows, fields] = await connection.execute('update users SET email=?, username=? WHERE id=?', [email, username, id]);
+        console.log(">>> check rows: ", rows);
+        return rows;
+    } catch (error) {
+        console.log(">>> check error: ", error);
+    }
+}
+
 module.exports = {
-    createNewUser, getUserList, deleteUser
+    createNewUser, getUserList, deleteUser, getUserById, updatUserInfor
 }
