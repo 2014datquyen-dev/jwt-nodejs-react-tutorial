fa5ddf9b65ee6878684df083fc032801b53d7926
Quyen Diep <2014datquyen@gmail.com>
2026-03-22 13:50:28 +0700

    video7.0 ✔ Fix lỗi cấu hình Sequelize ✔ Tạo mới users với Sequelize

--- src/service/userService.js
+++ src/service/userService.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 // Get the client
 import mysql from 'mysql2/promise';
 import bluebird from 'bluebird';
-
+import db from '../models/index';
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -12,13 +12,14 @@ const hashPassword = (userPassword) => {
 }
 
 const createNewUser = async (email, password, username) => {
-    let hashPass = hashPassword(password);
-    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
 
+    let hashPass = hashPassword(password);
     try {
-        const [rows, fields] =
-            await connection.execute('INSERT INTO user (email, password, username) VALUES (?, ?, ?)',
-                [email, hashPass, username]);
+        db.User.create({
+            username: username,
+            email: email,
+            password: hashPass
+        });
     } catch (error) {
         console.log(">>> check error: ", error);
     }
