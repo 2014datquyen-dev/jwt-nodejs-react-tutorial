1402c914cb3fae0301aa7b1e23d679b4f3d70fce
Quyen Diep <2014datquyen@gmail.com>
2026-03-22 22:16:36 +0700

    video8.4 ✔  Định nghĩa các mối quan hệ giữa các Models đã phân tích ✔ Tìm hiểu về Eager Loading ✔ Test dữ liệu với Eager Loading + Relationships

--- src/service/userService.js
+++ src/service/userService.js
@@ -28,6 +28,35 @@ const createNewUser = async (email, password, username) => {
 
 
 const getUserList = async () => {
+    //test relationship
+    let newUser = await db.User.findOne({
+        where: { id: 1 },
+        attributes: ["id", "username", "email"],
+        include: { model: db.Group, attributes: ["name", "description"] },
+        raw: true,
+        nest: true
+    })
+
+    // let roles = await db.Group.findOne({
+    //     include: [{ model: db.Role}],
+    //     raw: true,
+    //     nest: true
+    // })
+
+    let r = await db.Group.findAll({
+        include: { model: db.Role, where: { id: 1 } },
+        raw: true,
+        nest: true
+    })
+
+    console.log(">>> check newUser: ", newUser);
+    console.log(">>> check roles: ", r);
+
+
+
+
+
+
     // // get the promise implementation, we will use bluebird
     // // create the connection, specify bluebird as Promise
     // const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
